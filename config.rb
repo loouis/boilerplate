fbf8570ba576ee361e3ad9e58f1df8966dd325f7
Louis Smith <louis@ahoia.com>
2015-06-23 22:28:19 +0100

    Boilerplate - first commit

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'compass/import-once/activate'
+require 'susy'
+require 'compass'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
